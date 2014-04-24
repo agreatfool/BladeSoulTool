5f971cc2b64e1228ad20f03bdbfaf75bfb1bef4c
Jonathan Dai <nightghost5078@gmail.com>
2014-04-24 12:07:18 +0800

    Save.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,34 @@
+const RACE_KUN = 'KunN';
+const RACE_JIN = 'Jin';
+const RACE_GON = 'Gon';
+const RACE_LYN = 'Lyn';
+
+const GENDER_MALE   = 'M';
+const GENDER_FEMALE = 'F';
+
+var fs = require('fs');
+var os = require('os').platform();
+var path = require('path');
 
 module.exports = function(grunt) {
 
-    var Task_Default = function() {
+    var Task_Default = function () {
+
+        var done = this.async();
+
         var fs = require('fs');
 
-        var filePath = './00012442.upk';
+        var filePath = path.join('resources', '00012442.upk');
 
-        var rs = fs.createReadStream(filePath, {encoding: 'utf-8', bufferSize: 11});
+        var rs = fs.createReadStream(filePath, {encoding: 'hex', bufferSize: 11});
 
         var data = '';
-        rs.on("data", function (trunk){
+        rs.on("data", function(trunk) {
             data += trunk;
         });
-        rs.on("end", function () {
+        rs.on("end", function() {
             console.log(data);
+            done();
         });
 
     };
