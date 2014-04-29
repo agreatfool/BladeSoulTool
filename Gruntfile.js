9ab6b632f6a6c9501ead06e967eaedb9ff84f696
Jonathan Dai <nightghost5078@gmail.com>
2014-04-29 17:24:18 +0800

    Grunt entrance fixed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,5 @@
 "use strict";
 
-var fs = require('fs');
-var path = require('path');
-var Util = require('./src/util/bst_util.js');
-
 // Array Remove - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
     var rest = this.slice((to || from) + 1 || this.length);
@@ -31,11 +27,6 @@ module.exports = function(grunt) {
     var Task_Default = function() {
         this.async();
 
-        var crawler = new Crawler(grunt);
-        crawler.start(Crawler.PART_BODY);
-//        crawler.part = 'body';
-//        crawler.fetchPage("http://cha.17173.com/bns/fashion/2090682.html", 1, 'detail');
-
     };
 
     var Task_Crawler = function() { // --part=body
@@ -53,10 +44,26 @@ module.exports = function(grunt) {
         crawler.start(part);
     };
 
+    var Task_MeshParser = function() { // --part=body-mesh
+        var Parser = require('./src/mesh/bst_mesh_parser.js');
+
+        this.async();
+
+        var part = grunt.option('part');
+        if (typeof part === 'undefined' || part == null || part == '') {
+            grunt.log.error('[Grunt Task_MeshParser] Command line option "--part" not given, use default value: "body-mesh".');
+            part = Parser.PART_BODY;
+        }
+
+        var parser = new Parser(grunt);
+        parser.start(part);
+    };
+
     //-------------------------------------------------------------------------------------------
     // Tasks
     //-------------------------------------------------------------------------------------------
     grunt.registerTask('default', Task_Default);
     grunt.registerTask('crawler', Task_Crawler);
+    grunt.registerTask('parser', Task_MeshParser);
 
 };
