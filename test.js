6c16f9ea66f8eef9f60f90bce93a740e41740cfb
Jonathan Dai <nightghost5078@gmail.com>
2014-04-30 18:16:46 +0800

    Update.

--- test.js
+++ test.js
@@ -27,17 +27,26 @@ console.log('strUc.lcfist(): ' + strUc.lcfist());
 console.log('strLc.ucfirst(): ' + strLc.ucfirst());
 
 // download image
-var url = 'https://www.google.com.hk/images/srpr/logo11w.png';
-var filepath = './database/crawler/pics/dummy/google.png';
-console.log('dirname: ' + path.dirname(filepath));
-request.head(url, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
+var fileDownload = function(url, filepath, callback) {
+    console.log('Start to download file: ' + url);
 
-    var ws = fs.createWriteStream(filepath);
-    ws.on('error', function(err) { console.log(err); });
+    var errReport = function(err) {
+        console.log('Error in downloading: ' + url);
+        console.log(err);
+    };
 
-    request(url).pipe(ws).on('close', function() {
-        console.log('download done!');
+    request.head(url, function(err, res){
+        if (err) { errReport(err); }
+
+        var ws = fs.createWriteStream(filepath);
+        ws.on('error', function(err) { errReport(err); });
+
+        request(url).pipe(ws).on('close', function() {
+            console.log('File "' + url + '" downloaded: size: ' + res.headers['content-length']);
+            callback();
+        });
     });
+};
+fileDownload('https://www.google.com.hk/images/srpr/logo11w.png', './database/crawler/pics/dummy/google.png', function() {
+    console.log('done!');
 });
