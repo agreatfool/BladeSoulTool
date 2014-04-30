577838ed6953f6b24b7f91124c7f080ac260016d
Jonathan Dai <nightghost5078@gmail.com>
2014-04-30 21:34:02 +0800

    Update.

--- test.js
+++ test.js
@@ -25,28 +25,3 @@ String.prototype.lcfist = function() {
 
 console.log('strUc.lcfist(): ' + strUc.lcfist());
 console.log('strLc.ucfirst(): ' + strLc.ucfirst());
-
-// download image
-var fileDownload = function(url, filepath, callback) {
-    console.log('Start to download file: ' + url);
-
-    var errReport = function(err) {
-        console.log('Error in downloading: ' + url);
-        console.log(err);
-    };
-
-    request.head(url, function(err, res){
-        if (err) { errReport(err); }
-
-        var ws = fs.createWriteStream(filepath);
-        ws.on('error', function(err) { errReport(err); });
-
-        request(url).pipe(ws).on('close', function() {
-            console.log('File "' + url + '" downloaded: size: ' + res.headers['content-length']);
-            callback();
-        });
-    });
-};
-fileDownload('https://www.google.com.hk/images/srpr/logo11w.png', './database/crawler/pics/dummy/google.png', function() {
-    console.log('done!');
-});
