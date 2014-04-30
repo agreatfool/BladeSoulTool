0d2ba6e3fa125edf86d96505343d1fc5fccd5557
Jonathan Dai <nightghost5078@gmail.com>
2014-04-30 17:32:51 +0800

    Update.

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
+"use strict";
+
 var stackTrace = require('stack-trace');
 var err = new Error('something went wrong');
 var trace = stackTrace.parse(err);
 
 console.log(trace);
+
+var strUc = 'Name!';
+var strLc = 'name!';
+
+String.prototype.ucfirst = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+String.prototype.lcfist = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+};
+
+console.log('strUc.lcfist(): ' + strUc.lcfist());
+console.log('strUc.ucfirst(): ' + strUc.ucfirst());
