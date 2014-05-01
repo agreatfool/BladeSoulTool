be4da1bb264fa09a41d13fcf56b6087d21803939
Jonathan Dai <nightghost5078@gmail.com>
2014-05-01 23:59:25 +0800

    Add command line sample.

---
+++ resources/umodel/run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+REM umodel.exe -export -uncook -path=Y:\Games\ÌÚÑ¶ÓÎÏ·\½£Áé_ÌÚÑ¶\contents\bns\CookedPC -game=bns -out=output 00015491 
+
+umodel.exe -view -path=Y:\Games\ÌÚÑ¶ÓÎÏ·\½£Áé_ÌÚÑ¶\contents\bns\CookedPC -game=bns 00015491 -meshes & pause > nul
