04b4f86cd1fee1a146843489b8b7755799a8a15d
Jonathan Dai <nightghost5078@gmail.com>
2014-05-02 23:17:36 +0800

    To run bat with chinese characters, have to use non utf8 encoding.

--- resources/umodel/run.bat
+++ resources/umodel/run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-REM umodel.exe -export -uncook -path=Y:\Gamesè…¾è®¯æ¸¸æˆ\å‰‘çµ_è…¾è®¯\contents\bns\CookedPC -game=bns -out=output 00015491
+REM umodel.exe -export -uncook -path=Y:\Games\ÌÚÑ¶ÓÎÏ·\½£Áé_ÌÚÑ¶\contents\bns\CookedPC -game=bns -out=output 00015491
 
-umodel.exe -view -path=Y:\Games\è…¾è®¯æ¸¸æˆ\å‰‘çµ_è…¾è®¯\contents\bns\CookedPC -game=bns 00016621 -meshes & pause > nul
+umodel.exe -view -path=Y:\Games\ÌÚÑ¶ÓÎÏ·\½£Áé_ÌÚÑ¶\contents\bns\CookedPC -game=bns 00016621 -meshes & pause > nul
 
 REM umodel.exe -view -path=Z:\prog\nodejs\BladeSoulTool\resources\umodel -game=bns 00015491 -meshes & pause > nul
