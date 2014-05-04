ec2079567d9913a832674ecbd3d5481e2241d671
Jonathan Dai <nightghost5078@gmail.com>
2014-05-04 15:57:05 +0800

    Add bin scripts.

---
+++ run.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var timer = setInterval(function() {
+    console.log('done!');
+}, 1000);
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+forever start -o logs/forever.log -e logs/forever-error.log -a --minUptime=1000ms --spinSleepTime=1000ms -d -v -w run.js
