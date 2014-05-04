16e2aca7fa94bead4d8917f77754331ba28db4d4
Jonathan Dai <nightghost5078@gmail.com>
2014-05-04 17:55:40 +0800

    Express static app done.

--- express/app.js
+++ express/app.js
@@ -1,16 +1,21 @@
 "use strict";
 
+var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use(function(err, req, res, next){
+//noinspection FunctionWithInconsistentReturnsJS
+app.use('*', function(err, req, res, next) {
+    if (!err) {
+        return next();
+    }
     console.error(err.stack);
     res.send(500, 'An error has occurred. Please try again later.');
 });
 
-app.get('/', function(req, res){
-    res.send(' This is a test ');
-});
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/bower_components', express.static(path.join(__dirname, '../bower_components')));
+app.use('/database', express.static(path.join(__dirname, '../database')));
 
 var server = app.listen(35642, function() {
     console.log('Listening on port %d', server.address().port);
