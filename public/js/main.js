77c59645768c8959dc2e35df40543af4ef172bb8
Jonathan Dai <nightghost5078@gmail.com>
2014-05-04 22:10:36 +0800

    Update.

--- public/js/main.js
+++ public/js/main.js
@@ -18,72 +18,25 @@ function($routeProvider, $routeSegmentProvider) {
     $routeSegmentProvider.options.autoLoadTemplates = true;
     $routeSegmentProvider.options.strictMode = true;
     $routeSegmentProvider.
-        when("/body",                                   "body").
-        when("/body/module/:moduleName",                "body.apis").
-        when("/body/module/:moduleName/api/:apiName",   "body.apis.detail").
-        when("/face",                                   "face").
-        when("/hair",                                   "hair").
+        when("/body", "body").
+        when("/face", "face").
+        when("/hair", "hair").
         // BODY
-        segment("api", {
-            "templateUrl": SZ.VIEW_URL + "admin/api/home.html",
-            "controller": "Fs2ApiCtrl"
+        segment("body", {
+            "templateUrl": "view/body/home.html",
+            "controller": "BstBodyCtrl"
         }).
-        within().
-        segment("apis", {
-            "templateUrl": SZ.VIEW_URL + "admin/api/apis.html",
-            "controller": "Fs2ApiModuleCtrl",
-            "dependencies": ["moduleName"]
-        }).
-        within().
-        segment("detail", {
-            "templateUrl": SZ.VIEW_URL + "admin/api/detail.html",
-            "controller": "Fs2ApiDetailCtrl",
-            "dependencies": ["apiName"]
-        }).
-        up().up().
         // FACE
-        segment("data", {
-            "templateUrl": SZ.VIEW_URL + "admin/data/home.html",
-            "controller": "Fs2DataCtrl"
+        segment("face", {
+            "templateUrl": "view/face/home.html",
+            "controller": "BstFaceCtrl"
         }).
-        within().
-        segment("orm", {
-            "templateUrl": SZ.VIEW_URL + "admin/data/orm.html",
-            "controller": "Fs2DataOrmCtrl",
-            "dependencies": ["ormName"]
-        }).
-        up().
         // HAIR
-        segment("func", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/home.html",
-            "controller": "Fs2FuncCtrl"
-        }).
-        within().
-        segment("createUser", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/create.html",
-            "controller": "Fs2FuncCreateUserCtrl"
-        }).
-        segment("deleteUser", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/delete.html",
-            "controller": "Fs2FuncDeleteUserCtrl"
-        }).
-        segment("deleteCache", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/delete.html",
-            "controller": "Fs2FuncDeleteUserCacheCtrl"
-        }).
-        segment("flushCache", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/execute.html",
-            "controller": "Fs2FuncFlushCacheCtrl"
-        }).
-        segment("readExceptions", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/exception.html",
-            "controller": "Fs2FuncReadExceptions"
-        }).
-        segment("tailLog", {
-            "templateUrl": SZ.VIEW_URL + "admin/func/log.html",
-            "controller": "Fs2FuncTailLog"
+        segment("hair", {
+            "templateUrl": "view/hair/home.html",
+            "controller": "BstHairCtrl"
         });
-    $routeProvider.otherwise({redirectTo: "/api"});
+    $routeProvider.otherwise({redirectTo: "/body"});
 }]);
 
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -99,18 +52,28 @@ function ($scope, $routeSegment) {
     $scope.segment = $routeSegment;
 }]);
 
-// Fs2ApiCtrl
-controllers.controller("Fs2ApiCtrl", [
-    "$scope", "$routeSegment", "Fs2ApiService",
+// BstBodyCtrl
+controllers.controller("BstBodyCtrl", [
+    "$scope", "$routeSegment", "BstBodyService",
 function ($scope, $routeSegment, service) {
-    console.log("Fs2ApiCtrl loaded!");
+    console.log("BstBodyCtrl loaded!");
     $scope.segment = $routeSegment;
-    $scope.modules = null;
-    service.loadApiModules().then(function(data) {
-        if (typeof data === "object" && Object.keys(data).length > 0) {
-            $scope.modules = data;
-        }
-    });
+}]);
+
+// BstFaceCtrl
+controllers.controller("BstFaceCtrl", [
+    "$scope", "$routeSegment", "BstFaceService",
+function ($scope, $routeSegment, service) {
+    console.log("BstFaceCtrl loaded!");
+    $scope.segment = $routeSegment;
+}]);
+
+// BstHairCtrl
+controllers.controller("BstHairCtrl", [
+    "$scope", "$routeSegment", "BstHairService",
+function ($scope, $routeSegment, service) {
+    console.log("BstHairCtrl loaded!");
+    $scope.segment = $routeSegment;
 }]);
 
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -118,100 +81,17 @@ function ($scope, $routeSegment, service) {
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 var services = angular.module("BstApp.Services", []);
 
-// Fs2ApiService
-services.factory("Fs2ApiService", ["$http", "$q", function($http, $q) {
-    /**
-     * {
-         *     moduleName: {
-         *         apiName: [
-         *             0: {
-         *                 "name": paramName,
-         *                 "type": paramType
-         *             },
-         *             ...
-         *         ],
-         *         ...
-         *     },
-         *     ...
-         * }
-     */
-    var apiData = null;
+// BstBodyService
+services.factory("BstBodyService", ["$http", "$q", function($http, $q) {
+    return {};
+}]);
 
-    var loadApiModules = function() {
-        var deferred = $q.defer();
-        $http({
-            method: "POST",
-            url: SZ.REQUEST_URL,
-            data: app.util.buildAdminReqParams("loadApiModules"),
-            headers: {"Content-type": "application/x-www-form-urlencoded"}
-        }).success(function(result) {
-            if (app.util.handleResponse(result, false)) { // {moduleName: {}, ...}
-                apiData = result.msg[0];
-                deferred.resolve(Object.keys(apiData));
-            } else {
-                deferred.reject();
-            }
-        });
-        return SzSpin.run(deferred.promise);
-    };
+// BstFaceService
+services.factory("BstFaceService", ["$http", "$q", function($http, $q) {
+    return {};
+}]);
 
-    var loadModuleApis = function(moduleName) {
-        var deferred = $q.defer();
-        $http({
-            method: "POST",
-            url: SZ.REQUEST_URL,
-            data: app.util.buildAdminReqParams("loadModuleApis", [moduleName]),
-            headers: {"Content-type": "application/x-www-form-urlencoded"}
-        }).success(function(result) {
-            if (app.util.handleResponse(result, false)) { // {apiName: [], ...}
-                apiData[moduleName] = result.msg[0];
-                deferred.resolve(Object.keys(apiData[moduleName]));
-            } else {
-                deferred.reject();
-            }
-        });
-        return SzSpin.run(deferred.promise);
-    };
-
-    var loadApiDetail = function(moduleName, apiName) {
-        var deferred = $q.defer();
-        $http({
-            method: "POST",
-            url: SZ.REQUEST_URL,
-            data: app.util.buildAdminReqParams("loadApiDetail", [moduleName, apiName]),
-            headers: {"Content-type": "application/x-www-form-urlencoded"}
-        }).success(function(result) {
-            if (app.util.handleResponse(result, false)) { // [0: {"name": paramName, "type": paramType}, ...]
-                apiData[moduleName][apiName] = result.msg[0];
-                deferred.resolve(apiData[moduleName][apiName]);
-            } else {
-                deferred.reject();
-            }
-        });
-        return SzSpin.run(deferred.promise);
-    };
-
-    var callApi = function(apiName, params) {
-        var deferred = $q.defer();
-        $http({
-            method: "POST",
-            url: SZ.REQUEST_URL,
-            data: app.util.buildReqParams(apiName, params),
-            headers: {"Content-type": "application/x-www-form-urlencoded"}
-        }).success(function(result) {
-            if (app.util.handleResponse(result)) {
-                deferred.resolve(result);
-            } else {
-                deferred.reject();
-            }
-        });
-        return SzSpin.run(deferred.promise);
-    };
-
-    return {
-        "loadApiModules": loadApiModules,
-        "loadModuleApis": loadModuleApis,
-        "loadApiDetail": loadApiDetail,
-        "callApi": callApi
-    };
+// BstHairService
+services.factory("BstHairService", ["$http", "$q", function($http, $q) {
+    return {};
 }]);
