9bb181e017c63d0b2c97bda13839d3e0023b751c
Jonathan Dai <nightghost5078@gmail.com>
2014-05-04 15:45:01 +0800

    Add install script.

---
+++ bin/install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set NODE_FOUND=
+for %%X in (node.exe) do (set NODE_FOUND=%%~$PATH:X)
+if "%NODE_FOUND%" == "" (
+    echo Command node not found, please install it first.
+    pause > nul
+) else (
+    echo Command node detected!
+)
+
+npm install -g grunt-cli forever & pause > nul
