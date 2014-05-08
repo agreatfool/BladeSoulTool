deed5db683c5852dde9d21315da13883075400e9
Jonathan Dai <nightghost5078@gmail.com>
2014-05-09 01:19:24 +0800

    Update.

--- test.bat
+++ test.bat
@@ -1,3 +1,8 @@
 @echo off
 
-grunt parser --stack & pause > nul
+REM cd express
+REM node app.js
+
+REM grunt replace --part=body --model=60077_JinF_col3 --stack & pause > nul
+
+grunt restore --stack & pause > nul
