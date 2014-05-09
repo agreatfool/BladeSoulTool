cb8b8952b26001aa488df3101f04b91808b37ba8
Jonathan Dai <nightghost5078@gmail.com>
2014-05-10 00:08:10 +0800

    Update.

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ..
-zip -9 -r BladeSoulTool.zip BladeSoulTool -x "BladeSoulTool/**/.DS_Store" "BladeSoulTool/**/*.zip" "BladeSoulTool/.git/*" "BladeSoulTool/resources/dedat/output/*" "BladeSoulTool/resources/umodel/output/*"
+zip -9 -r ~/Downloads/BladeSoulTool.zip BladeSoulTool -b "/tmp" -x "BladeSoulTool/**/.DS_Store" "BladeSoulTool/**/*.zip" "BladeSoulTool/.git/*" "BladeSoulTool/resources/dedat/output/*" "BladeSoulTool/resources/umodel/output/*"
