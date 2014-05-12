0c623a89e0c01e499e9cddea0a4d57fc7016e06b
Jonathan Dai <nightghost5078@gmail.com>
2014-05-12 15:12:22 +0800

    Fix wrong command.

--- bin/replace.bat
+++ bin/replace.bat
@@ -4,4 +4,4 @@ set /p part="Enter part [body|face|hair]: " %=%
 set /p model="Enter modelId: " %=%
 
 cd ..
-grunt parser --part=%part% --model=%model% --stack & pause > nul
+grunt replace --part=%part% --model=%model% --stack & pause > nul
