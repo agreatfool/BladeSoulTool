17dad39b539b6b08f1faea987f5ac6b41f1a92a9
Jonathan Dai <nightghost5078@gmail.com>
2014-05-16 15:54:24 +0800

    Save.

--- src/upk/bst_upk_parser.js
+++ src/upk/bst_upk_parser.js
@@ -18,13 +18,19 @@ var BstUpkParser = function(grunt) {
     this.grunt  = grunt;
     this.util   = new BstUtil(grunt);
 
-    /**
-     *
-     */
-    this.upks = {}; // 暂存的upk数据
+    // upk列表数据，格式皆为 { upkLogFileName : upkLogContentLineThree, ... }
+    this.upkListSkeleton = {};
+    this.upkListTexture = {};
+    this.upkListMaterial = {};
+    this.upkListUnrecognized = {}; // BstUpkParser.UPK_TYPE_* 可识别的upk类型之外的upk
 };
 
+BstUpkParser.PATH_UPK_BASE = 'database/upk';
+BstUpkParser.PATH_UPK_LOG = path.join(BstUpkParser.PATH_UPK_BASE, 'log');
+BstUpkParser.PATH_UPK_DATA = path.join(BstUpkParser.PATH_UPK_BASE, 'data');
+
 BstUpkParser.ENTRANCE_LINE_NO = 2; // 一般来说，log的第三行里的内容是关键信息
+
 BstUpkParser.UPK_TYPE_SKELETON = 'SkeletalMesh3';
 BstUpkParser.UPK_TYPE_TEXTURE  = 'Texture2D';
 BstUpkParser.UPK_TYPE_MATERIAL = 'MaterialInstanceConstant';
@@ -41,25 +47,34 @@ BstUpkParser.prototype.start = function() {
             self.process(filename, abspath);
         }
     });
-    self.util.writeFile('database/costume/upk_data/upk_raw_list.json', self.util.formatJson(self.upks));
+
+    // 写入各分类的列表信息
+    self.util.writeFile(path.join(BstUpkParser.PATH_UPK_DATA, 'list_skeleton.json'), self.util.formatJson(self.upkListSkeleton));
+    self.util.writeFile(path.join(BstUpkParser.PATH_UPK_DATA, 'list_texture.json'), self.util.formatJson(self.upkListTexture));
+    self.util.writeFile(path.join(BstUpkParser.PATH_UPK_DATA, 'list_material.json'), self.util.formatJson(self.upkListMaterial));
+    self.util.writeFile(path.join(BstUpkParser.PATH_UPK_DATA, 'list_unrecognized.json'), self.util.formatJson(self.upkListUnrecognized));
 };
 
 BstUpkParser.prototype.process = function(filename, logPath) {
     this.grunt.log.writeln('[BstUpkParser] Start to parse upk file: ' + logPath);
     var content = this.util.readFile(logPath).toString().split("\r\n");
-    this.upks[filename] = content[2];
 
     if (content[2].match(new RegExp(BstUpkParser.UPK_TYPE_SKELETON)) !== null) {
+        this.upkListSkeleton[filename] = content[2];
         this.processSkeleton(filename, logPath, content);
     } else if (content[2].match(new RegExp(BstUpkParser.UPK_TYPE_TEXTURE)) !== null) {
+        this.upkListTexture[filename] = content[2];
         this.processTexture(filename, logPath, content);
     } else if (content[2].match(new RegExp(BstUpkParser.UPK_TYPE_MATERIAL)) !== null) {
+        this.upkListMaterial[filename] = content[2];
         this.processMaterial(filename, logPath, content);
+    } else {
+        this.upkListUnrecognized[filename] = content[2];
     }
 };
 
 BstUpkParser.prototype.processSkeleton = function(filename, logPath, content) {
-    var self = this;
+
 };
 
 BstUpkParser.prototype.processTexture = function(filename, logPath, content) {
