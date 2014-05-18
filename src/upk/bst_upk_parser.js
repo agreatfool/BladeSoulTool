82739687ef3989464c4a5525fd43df0c4ba6ca08
Jonathan Dai <nightghost5078@gmail.com>
2014-05-18 14:18:30 +0800

    Remove old logic.

--- src/upk/bst_upk_parser.js
+++ src/upk/bst_upk_parser.js
@@ -17,14 +17,6 @@ var BstConst = require('../const/bst_const.js');
 var BstUpkParser = function(grunt) {
     this.grunt  = grunt;
     this.util   = new BstUtil(grunt);
-
-    // upk列表数据，格式皆为 { upkLogFileName : upkLogContentLineThree, ... }
-    this.upkListSkeleton = {};
-    this.upkListSkeleton_body = {};
-    this.upkListSkeleton_unrecognized = {};
-    this.upkListTexture = {};
-    this.upkListMaterial = {};
-    this.upkListUnrecognized = {}; // BstUpkParser.UPK_TYPE_* 可识别的upk类型之外的upk
 };
 
 BstUpkParser.prototype.start = function() {
@@ -34,48 +26,14 @@ BstUpkParser.prototype.start = function() {
     self.grunt.log.writeln('[BstUpkParser] Start to parse upk files ...');
     self.util.printHr();
 
-    self.grunt.file.recurse(BstConst.PATH_UPK_LOG, function(abspath, rootdir, subdir, filename) {
-        if (filename !== 'upk_dir') {
-            self.process(filename, abspath);
-        }
-    });
-
-    // 写入各分类的列表信息
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_skeleton.json'), self.util.formatJson(self.upkListSkeleton));
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_skeleton_body.json'), self.util.formatJson(self.upkListSkeleton_body));
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_skeleton_unrecognized.json'), self.util.formatJson(self.upkListSkeleton_unrecognized));
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_texture.json'), self.util.formatJson(self.upkListTexture));
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_material.json'), self.util.formatJson(self.upkListMaterial));
-    self.util.writeFile(path.join(BstConst.PATH_UPK_DATA, 'list_unrecognized.json'), self.util.formatJson(self.upkListUnrecognized));
 };
 
 BstUpkParser.prototype.process = function(filename, logPath) {
-    this.grunt.log.writeln('[BstUpkParser] Start to parse upk file: ' + logPath);
-    var content = this.util.readFile(logPath).toString().split("\r\n");
 
-    if (content[2].match(new RegExp(BstConst.UPK_TYPE_SKELETON)) !== null) {
-        this.upkListSkeleton[filename] = content[2];
-        this.processSkeleton(filename, logPath, content);
-    } else if (content[2].match(new RegExp(BstConst.UPK_TYPE_TEXTURE)) !== null) {
-        this.upkListTexture[filename] = content[2];
-        this.processTexture(filename, logPath, content);
-    } else if (content[2].match(new RegExp(BstConst.UPK_TYPE_MATERIAL)) !== null) {
-        this.upkListMaterial[filename] = content[2];
-        this.processMaterial(filename, logPath, content);
-    } else {
-        this.upkListUnrecognized[filename] = content[2];
-    }
 };
 
 BstUpkParser.prototype.processSkeleton = function(filename, logPath, content) {
-    var category = null;
-    if (content[2].match(/\d+_(KunN|JinF|JinM|GonF|GonM|LynF|LynM)/i) !== null) {
-        category = 'body';
-        this.upkListSkeleton_body[filename] = content[2];
-    } else {
-        category = 'unrecognized';
-        this.upkListSkeleton_unrecognized[filename] = content[2];
-    }
+
 };
 
 BstUpkParser.prototype.processTexture = function(filename, logPath, content) {
