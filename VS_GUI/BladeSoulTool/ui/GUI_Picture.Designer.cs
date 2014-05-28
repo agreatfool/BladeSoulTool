87bb1c0fb82c88be334b928d1267b39eae2643a1
Jonathan Dai <nightghost5078@gmail.com>
2014-05-28 15:19:06 +0800

    Add picture display form.

---
+++ VS_GUI/BladeSoulTool/ui/GUI_Picture.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BladeSoulTool.ui
+{
+    partial class GUI_Picture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2D = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2D)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2D
+            // 
+            this.pictureBox2D.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2D.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2D.Name = "pictureBox2D";
+            this.pictureBox2D.Size = new System.Drawing.Size(500, 600);
+            this.pictureBox2D.TabIndex = 0;
+            this.pictureBox2D.TabStop = false;
+            // 
+            // GUI_Picture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 625);
+            this.Controls.Add(this.pictureBox2D);
+            this.Name = "GUI_Picture";
+            this.Text = "Picture";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2D)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2D;
+    }
+}
---
+++ VS_GUI/BladeSoulTool/ui/GUI_Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BladeSoulTool.ui
+{
+    public partial class GUI_Picture : Form
+    {
+        public GUI_Picture(string imgPath)
+        {
+            InitializeComponent();
+
+            this.pictureBox2D.ImageLocation = imgPath;
+            this.pictureBox2D.Load();
+        }
+    }
+}
