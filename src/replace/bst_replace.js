6dbd690f287a646ae70e1d7bde4dedb9c247a7c2
Jonathan Dai <nightghost5078@gmail.com>
2014-06-05 22:04:07 +0800

    Fix replace script with latest database structure.

--- src/replace/bst_replace.js
+++ src/replace/bst_replace.js
@@ -15,88 +15,98 @@ var BstConst = require('../const/bst_const.js');
 var BstReplace = function(grunt) {
     this.grunt = grunt;
     this.util = new BstUtil(grunt);
+    this.gruntWorkingPath = process.cwd();
 
     this.backup = this.util.readJsonFile('./config/backup.json');
 
-    this.part = null; // 当前替换的是哪个部分的模型：body、face、hair
-
-    this.data = {}; // 模型数据：database/costume/[this.part]/data.json
-    this.base = {}; // 当前指定部分的基础配置：database/costume/[this.part]/base.json
-    this.modelId = null; // 当前工作需要替换的目标模型id
+    this.part = null; // 当前替换的是哪个部分的模型：costume、attach、weapon
+    this.targetModelInfo = {}; // 目标模型数据，来自：database/[costume|attach|weapon]/data/data.json
+    this.originModelInfo = {}; // 原始模型数据，来自：database/[costume|attach|weapon]/data/origin.json
 };
 
-BstReplace.prototype.start = function(part, modelId) {
+BstReplace.prototype.start = function(part, race, modelId) {
     this.util.printHr();
-    if ([BstConst.PART_BODY, BstConst.PART_FACE, BstConst.PART_HAIR].indexOf(part) === -1) {
-        this.grunt.fail.fatal('[BstReplace] Invalid start part specified: ' + part);
-    }
+    this.grunt.log.writeln('[BstReplace] Start to replace model of part: "' + part + '", race: "' +
+        race + '", targetModelId: "' + modelId);
+    this.util.printHr();
+
+    this.util.partTypeCheck(part);
 
     this.part = part;
-    this.data = this.util.readJsonFile('./database/costume/' + this.part + '/data.json');
-    this.base = this.util.readJsonFile('./database/costume/' + this.part + '/base.json');
-    var modelIds = _.keys(this.data);
-    if (!modelIds.indexOf(modelId)) {
-        this.grunt.fail.fatal('[BstReplace] Invalid model id specified: ' + modelId);
-    }
-    this.modelId = modelId;
+    var dataCollection = this.util.readJsonFile('./database/' + this.part + '/data/data.json');
+    var modelIds = _.keys(dataCollection);
 
-    this.grunt.log.writeln('[BstReplace] Start to replace model of part: ' + part);
-    this.util.printHr();
+    // 确定替换目标模型数据
+    if (!modelIds.indexOf(modelId)) {
+        this.grunt.fail.fatal('[BstReplace] Invalid target model id specified: ' + modelId);
+    } else {
+        this.targetModelInfo = dataCollection[modelId];
+        self.grunt.log.writeln('[BstReplace] Target model info read: ' + self.util.formatJson(this.targetModelInfo));
+    }
+
+    // 确定原始模型数据
+    var originCollection = this.util.readJsonFile('./database/' + this.part + '/data/origin.json');
+    if (part === BstConst.PART_TYPE_COSTUME || part === BstConst.PART_TYPE_ATTACH) { // 替换时装 或 饰品
+        if (race === null) { // 没有提供种族信息
+            this.grunt.fail.fatal('[BstReplace] No race info given when replace costume | attach ...');
+        } else {
+            if (!originCollection.hasOwnProperty(race) || !originCollection[race].hasOwnProperty('data')) { // 没有找到原始模型信息
+                this.grunt.fail.fatal('[BstReplace] No origin model info set ...');
+            } else {
+                this.originModelInfo = originCollection[race]['data'];
+                self.grunt.log.writeln('[BstReplace] Origin model info read: ' + self.util.formatJson(this.originModelInfo));
+            }
+        }
+    } else {
+        if (!originCollection.hasOwnProperty('data')) { // 没有找到原始模型信息
+            this.grunt.fail.fatal('[BstReplace] No origin model info set ...');
+        } else {
+            this.originModelInfo = originCollection['data'];
+            self.grunt.log.writeln('[BstReplace] Origin model info read: ' + self.util.formatJson(this.originModelInfo));
+        }
+    }
 
     this.process();
 };
 
 BstReplace.prototype.process = function() {
     switch (this.part) {
-        case BstConst.PART_BODY:
-            this.processBody();
+        case BstConst.PART_TYPE_COSTUME:
+            this.processCostume();
             break;
-        case BstConst.PART_FACE:
-            this.processFace();
+        case BstConst.PART_TYPE_ATTACH:
+            this.processAttach();
             break;
-        case BstConst.PART_HAIR:
-            this.processHair();
+        case BstConst.PART_TYPE_WEAPON:
+            this.processWeapon();
             break;
         default:
             break;
     }
 };
 
-BstReplace.prototype.processBody = function() {
+BstReplace.prototype.processCostume = function() {
     var self = this;
-
-    var raceMatch = self.modelId.match(/(KunN|JinF|JinM|GonF|GonM|LynF|LynM)/);
-    if (raceMatch === null) {
-        self.grunt.fail.fatal('[BstReplace] Invalid modelId, no race info found in it: ' + self.modelId);
-    }
-    var race = raceMatch[0];
-
-    // 读取目标模型配置
-    var element = self.data[race][self.modelId];
-    self.grunt.log.writeln('[BstReplace] Target conf read: ' + self.util.formatJson(element));
-    // 读取基本模型配置
-    var base = self.base[element['race']];
-    self.grunt.log.writeln('[BstReplace] Base conf read: ' + self.util.formatJson(base));
 
     // 准备目标模型upk路径，并验证存在
     var paths = {
-        "skeleton": self.util.findUpkPath(element['skeleton']),
-        "texture":  self.util.findUpkPath(element['texture']),
-        "material": self.util.findUpkPath(element['material'])
+        "skeleton": self.util.findUpkPath(self.targetModelInfo['skeleton']),
+        "texture":  self.util.findUpkPath(self.targetModelInfo['texture']),
+        "material": self.util.findUpkPath(self.targetModelInfo['material'])
     };
     for (var checkKey in paths) {
         if (!paths.hasOwnProperty(checkKey)) {
             continue;
         }
         if (paths[checkKey] === null) { // 如果upk没找到，直接失败，并报错给子进程，这样才能返回信息给客户端
-            self.grunt.fail.fatal('[BstReplace] Target upk not found: ' + base[checkKey] + '.upk');
+            self.grunt.fail.fatal('[BstReplace] upk file of target model not found, id: ' + self.targetModelInfo[checkKey] + '.upk');
         }
     }
-    self.grunt.log.writeln('[BstReplace] Base upk path found: ' + self.util.formatJson(paths));
+    self.grunt.log.writeln('[BstReplace] All upk files of target model found: ' + self.util.formatJson(paths));
 
-    // 拷贝目标upk到working路径下，并重命名为洪门道服的upk名
+    // 拷贝目标upk到working路径下，并重命名为目标模型的upk名
     _.each(paths, function(copyPath, copyKey) {
-        var workingPath = path.join('working', base[copyKey] + '.upk');
+        var workingPath = path.join('working', self.originModelInfo[copyKey] + '.upk');
         self.util.copyFile(copyPath, workingPath);
         paths[copyKey] = workingPath;
     });
@@ -108,19 +118,19 @@ BstReplace.prototype.processBody = function() {
         }
         self.util.registerAsyncEvent(paths[editKey]);
         self.util.readHexFile(paths[editKey], function(data, editPath) {
-            // 拷贝过来的文件内原来的：目标服装模型名 => 改为洪门道服的模型名
-            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(element['codeWithRace']), self.util.strUtf8ToHex(base['codeWithRace']));
+            // 拷贝过来的文件内原来的：目标服装模型名 => 改为原始服装的模型名
+            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(self.targetModelInfo['core']), self.util.strUtf8ToHex(self.originModelInfo['core']));
             // 替换各upk的id名，包括texture, material, skeleton
-            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(element['texture']), self.util.strUtf8ToHex(base['texture']));
-            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(element['material']), self.util.strUtf8ToHex(base['material']));
-            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(element['skeleton']), self.util.strUtf8ToHex(base['skeleton']));
+            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(self.targetModelInfo['texture']), self.util.strUtf8ToHex(self.originModelInfo['texture']));
+            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(self.targetModelInfo['material']), self.util.strUtf8ToHex(self.originModelInfo['material']));
+            data = self.util.replaceStrAll(data, self.util.strUtf8ToHex(self.targetModelInfo['skeleton']), self.util.strUtf8ToHex(self.originModelInfo['skeleton']));
             // 如果是多色模型的material的话，还需要修改当前模型的col
             var editPart = _.keys(paths.findByVal(editPath)).shift();
-            if (editPart == 'material' && 'col1' !== element['col']) {
+            if (editPart == 'material' && 'col1' !== self.targetModelInfo['col']) {
                 data = self.util.replaceStrLast(
                     data,
-                    self.util.strUtf8ToHex(element['col']), // 原始：替换目标模型的col
-                    self.util.strUtf8ToHex('col1') // 目标：洪门道服永远是 col1
+                    self.util.strUtf8ToHex(self.targetModelInfo['col']), // 原始：替换目标模型的col
+                    self.util.strUtf8ToHex('col1') // 目标：永远是 col1，只允许替换为col1的服装
                 );
             }
             // 写入到working文件内
@@ -142,7 +152,7 @@ BstReplace.prototype.processBody = function() {
             /**
              * 因为只有一层文件夹结构，所以不用担心多层嵌套问题
              */
-            if (filename === 'working_dir') { return; } // 忽略文件夹占位文件
+            if (filename === 'working_dir') { return; } // 忽略占位文件
             var targetTencentPath = path.join(self.util.getTencentPath(), filename);
             self.util.copyFile(abspath, targetTencentPath);
             self.util.deleteFile(abspath);
@@ -155,11 +165,11 @@ BstReplace.prototype.processBody = function() {
     });
 };
 
-BstReplace.prototype.processFace = function() {
+BstReplace.prototype.processAttach = function() {
 
 };
 
-BstReplace.prototype.processHair = function() {
+BstReplace.prototype.processWeapon = function() {
 
 };
 
