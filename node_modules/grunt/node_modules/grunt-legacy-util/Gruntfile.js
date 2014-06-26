c7b436cd5d384a75c14c5ba730b96d46144059d1
Jonathan Dai <nightghost5078@gmail.com>
2014-06-26 13:48:14 +0800

    Update npm package grunt.

--- node_modules/grunt/node_modules/grunt-legacy-util/Gruntfile.js
+++ node_modules/grunt/node_modules/grunt-legacy-util/Gruntfile.js
@@ -3,14 +3,28 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+      },
+      all: ['*.js', 'test/*.js'],
+    },
     nodeunit: {
       util: ['test/index.js']
     },
-
+    watch: {
+      all: {
+        files: ['<%= jshint.all %>'],
+        tasks: ['test'],
+      },
+    },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['nodeunit']);
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+  grunt.registerTask('default', ['test', 'watch']);
 
 };
